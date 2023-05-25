7b12dd876a8488d789f6a1f4401ee609ff0d7ca5
Aminul667 <rahathssn@gmail.com>
2023-05-25 14:38:55 +0600

    75-4 Display different menu category items with cover

--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,10 +1,17 @@
+import Cover from "../../Shared/Cover/Cover";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const MenuCategory = ({ items, title, img }) => {
+  return (
+    <div className="pt-8">
+    {title && <Cover img={img} title={title}></Cover>}
+      <div className="grid grid-cols-2 gap-6 my-16">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;
