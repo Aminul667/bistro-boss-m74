384c785744480b7bd66fe9d8e7634b46b39eaee5
Aminul667 <rahathssn@gmail.com>
2023-05-25 18:07:32 +0600

    added reviews data

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -7,13 +7,13 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { useEffect, useState } from "react";
 import { Rating } from "@smastrom/react-rating";
-import '@smastrom/react-rating/style.css'
+import "@smastrom/react-rating/style.css";
 
 const Testimonial = () => {
   const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
-    fetch("reviews.json")
+    fetch("http://localhost:5000/reviews")
       .then((res) => res.json())
       .then((data) => setReviews(data));
   }, []);
@@ -28,7 +28,11 @@ const Testimonial = () => {
         {reviews.map((review) => (
           <SwiperSlide key={review._id}>
             <div className="flex flex-col items-center mx-24 my-16">
-              <Rating style={{ maxWidth: 180 }} value={review.rating} readOnly />
+              <Rating
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
               <p className="py-8">{review.details}</p>
               <h3 className="text-2xl text-orange-400">{review.name}</h3>
             </div>
