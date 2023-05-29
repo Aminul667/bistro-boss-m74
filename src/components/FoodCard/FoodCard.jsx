55cf1958af0f66017a20d32cddfc8e9cb09ff852
Aminul667 <rahathssn@gmail.com>
2023-05-29 22:07:14 +0600

    77-2 API naming convention and API to save Cart

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,23 +1,64 @@
+import { useContext } from "react";
+import { AuthContext } from "../../providers/Authprovider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
 const FoodCard = ({ item }) => {
   const { name, image, price, recipe } = item;
-  const handleAddToCart = item => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleAddToCart = (item) => {
     console.log(item);
-  }
+    if (user) {
+      fetch("http://localhost:5000/")
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.insertedId) {
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Your work has been saved",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        });
+    } else {
+      Swal.fire({
+        title: "Please login to order the food",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Login now!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate("/login");
+        }
+      });
+    }
+  };
 
   return (
     <div>
       <div className="card w-96 bg-base-100 shadow-xl">
         <figure>
-          <img
-            src={image}
-          />
+          <img src={image} />
         </figure>
-        <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4 px-4">${price}</p>
+        <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4 px-4">
+          ${price}
+        </p>
         <div className="card-body flex flex-col items-center">
           <h2 className="card-title">{name}</h2>
           <p>{recipe}</p>
           <div className="card-actions justify-end">
-            <button onClick={() => handleAddToCart(item)} className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100 border-orange-400">Add to Cart</button>
+            <button
+              onClick={() => handleAddToCart(item)}
+              className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100 border-orange-400"
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
