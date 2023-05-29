7005aeb42a5ba1c841cb0f7582c6cd4bb78476f8
Aminul667 <rahathssn@gmail.com>
2023-05-29 21:38:10 +0600

    77-1 Module Introduction

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../providers/Authprovider";
+import { FaShoppingCart } from 'react-icons/fa';
 
 const NavBar = () => {
   const { user, logOut } = useContext(AuthContext);
@@ -15,22 +16,37 @@ const NavBar = () => {
     <>
       <li>
         <Link to="/">Home</Link>
-        <Link to="/menu">Our Menu</Link>
-        <Link to="/order/salad">Order Food</Link>
-        <Link to="secret">Secret</Link>
-        {user ? (
-          <>
-            <span>{user?.displayName}</span>
-            <button onClick={handleLogOut} className="btn btn-ghost">
-              Log Out
-            </button>
-          </>
-        ) : (
-          <>
-            <Link to="/login">Login</Link>
-          </>
-        )}
       </li>
+      <li>
+        <Link to="/menu">Our Menu</Link>
+      </li>
+      <li>
+        <Link to="/order/salad">Order Food</Link>
+      </li>
+      <li>
+        <Link to="secret">Secret</Link>
+      </li>
+      <li>
+        <Link to="/">
+          <button className="btn gap-2">
+            <FaShoppingCart></FaShoppingCart>
+            <div className="badge badge-secondary">+0</div>
+          </button>
+        </Link>
+      </li>
+      {user ? (
+        <>
+          <button onClick={handleLogOut} className="btn btn-ghost">
+            Log Out
+          </button>
+        </>
+      ) : (
+        <>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </>
+      )}
     </>
   );
 
